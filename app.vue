b49d777006571b12fc73f6e8e5b3a15c4caff154
manvfx <manvfx@gmail.com>
2023-05-13 01:23:28 +0330

    add tailwindcss

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="h-screen w-screen flex content-center justify-center items-center bg-gradient-to-r from-violet-200 to-pink-200">
+    <h1 class="text-lg border-2 border-solid border-black hover:border-pink-500 text-black hover:text-pink-500 cursor-pointer rounded-lg px-4 py-3">Hello Super Frontend!</h1>
   </div>
 </template>
